20ff76e869206448191f18b1295c64d7afb5c390
Bradley Torok <peoplesoft@me.com>
2010-11-22 17:46:24 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Peoplesoft::Application.config.secret_token = 'c8049d05d0f04edbbf733b028ea948d5fec73d9c2523b09a0500435e4bea160865028fb1ee8431d67a4aa179f77c71d0da4a2cfe59598b345506075306ba69a0'
